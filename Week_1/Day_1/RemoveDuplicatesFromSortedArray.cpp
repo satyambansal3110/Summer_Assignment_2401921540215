8009ccfa5acaa48a9fb9dc11e58fcc4bea1c8780
Satyam Bansal <satyambansal3110@gmail.com>
2026-06-01 11:20:53 +0530

    Create RemoveDuplicatesFromSortedArray.cpp

---
+++ Week_1/Day_1/RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        vector<int>ans;
+        ans.push_back(nums[0]); 
+        for(int i=0;i<nums.size();i++) 
+        {        
+            if( i>0 && nums[i] != nums[i-1]){
+                ans.push_back(nums[i]);
+            }
+        }
+        nums=ans;
+
+        return nums.size();
+        
+    }
+};
