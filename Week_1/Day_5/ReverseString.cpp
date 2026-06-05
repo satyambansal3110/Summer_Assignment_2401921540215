ee0be2f349b829716d7d64eff62538be97b73097
Satyam Bansal <satyambansal3110@gmail.com>
2026-06-05 12:43:22 +0530

    Create ReverseString.cpp

---
+++ Week_1/Day_5/ReverseString.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<char> reverseString(vector<char>& s) {
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+        return s;
+    }
+};
