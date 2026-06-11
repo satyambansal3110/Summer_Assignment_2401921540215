d99010e0714128650abc57adcf0746325a8ba8b0
Satyam Bansal <satyambansal3110@gmail.com>
2026-06-11 14:02:32 +0530

    Create DecodeString.cpp

---
+++ Week_2/Day_4/DecodeString.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        int i=0;
+        return decode(s,i);
+        
+    }
+    string decode(string &s,int &i){
+             
+    string result="";
+        int num=0;
+        while( i<s.size()){
+            char c=s[i];
+            if (isdigit(c)) {
+            num = num * 10 + (c - '0'); 
+              i++;
+        } else if (c == '[') {
+          i++;
+          string inner = decode(s, i); 
+          for (int k = 0; k < num; k++) result += inner;
+          num = 0; 
+        } else if (c == ']') {
+          i++; 
+          return result; 
+        } else {
+          result += c; 
+          i++;
+        }
+      }
+      return result;
+
+    }
+};
