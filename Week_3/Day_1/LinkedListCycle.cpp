c5608651baf4efe1e166df96ae34734d509f83ee
Satyam Bansal <satyambansal3110@gmail.com>
2026-06-15 17:30:01 +0530

    Create LinkedListCycle.cpp

---
+++ Week_3/Day_1/LinkedListCycle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *s=head;
+        ListNode *f=head;
+        while(f != NULL && f->next != NULL){
+            s=s->next;
+            f=f->next->next;
+            if(s==f) return true;
+        }
+        return false;
+    }
+};
