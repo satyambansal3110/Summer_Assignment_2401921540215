5d39995bf779f4e9f1feba28bc42becee727a60c
Satyam Bansal <satyambansal3110@gmail.com>
2026-06-16 12:06:38 +0530

    Create MergeTwoSortedList.cpp

---
+++ Week_3/Day_2/MergeTwoSortedList.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy;
+        ListNode* p=&dummy;
+        ListNode* p1=list1;
+        ListNode* p2=list2;
+        while(p1 !=NULL && p2!=NULL){
+            if(p1->val<=p2->val){
+                p->next=p1;
+                p1=p1->next;
+                
+            } else{
+               p->next=p2;
+               p2=p2->next;
+            }
+            p=p->next;
+        }
+        if(p1!=NULL) p->next=p1;
+        else p->next=p2;
+        return dummy.next;
+    }
+};
